4a28c49e56908925f7c2091cf58ea9fed0e4e5a7
Ali Haroon <112351004+Ali-Haroon3@users.noreply.github.com>
2025-06-16 22:28:30 -0600

    aws changes

--- app.py
+++ app.py
@@ -8,4 +8,5 @@ from simple_complete_app import app
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8000))
-    app.run(host='0.0.0.0', port=port, debug=False)
+    print(f"Starting Flask app on port {port}")
+    app.run(host='0.0.0.0', port=port, debug=False, threaded=True)
